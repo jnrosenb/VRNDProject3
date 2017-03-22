35bab098c8ff115bdc34f7b434c6fefd0711ec0b
José Rosenbluth <Jos Rosenbluth>
2017-03-22 05:45:54 -0400

    update: Added lightning for scene, and also waypoints in maze

---
+++ Assets/fountain.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fountain : MonoBehaviour {
+
+	[Header("Audio Source")]
+	public AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource.playOnAwake = true;
+		audioSource.spatialBlend = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
